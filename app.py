7514d0ff9f924a5f36b9a3977b28d59e950fb564
Enilson Rocha <enilsonpe@gmail.com>
2026-06-12 16:38:08 -0300

    Adicionado o Gráfico Dispersão com Butão

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
+# Criando a página de análise de veículos com Streamlit
 st.set_page_config(
     page_title="Análise de Veículos",
     page_icon="🚗",
@@ -24,9 +25,10 @@ st.markdown("<p class='big-title'>🚗 Análise de Veículos</p>",
 # Carregar dados
 car_data = pd.read_csv("vehicles.csv")  # lendo os dados
 
-# Botão para gerar o gráfico de histograma
+# Criando Botão para gerar o gráfico de histograma
 hist_button = st.button('Criar histograma', key="hist1")  # criar um botão
 
+# Verifica se o botão do histograma foi clicado
 if hist_button:
     # escrever uma mensagem
     st.write(
@@ -34,4 +36,42 @@ if hist_button:
     # criar um histograma
     fig = px.histogram(car_data, x="odometer")
     # exibir um gráfico Plotly interativo
+    st.plotly_chart(fig, use_container_width=True)
+
+
+""" Criando um gráfico de dispersão para analisar a relação entre preço e quilometragem 
+    dos veículos """
+
+# Estilo do título gráfico de dispersão
+st.markdown("""
+    <style>
+        .custom-title {
+            font-size: 36px !important;
+            font-weight: bold !important;
+        }
+    </style>
+""", unsafe_allow_html=True)
+
+st.markdown("<p class='custom-title'>Gráfico de Dispersão — Preço vs Quilometragem</p>",
+            unsafe_allow_html=True)
+
+# Criando Botão para gerar o gráfico de dispersão
+hist_button2 = st.button('Criar de Dispersão', key="hist2")  # criar um botão
+
+# Verifica se o botão da dispersão foi clicado
+if hist_button2:
+    # Gráfico
+    fig = px.scatter(
+        car_data,
+        x="odometer",
+        y="price",
+        color="condition",
+        title="Preço vs Quilometragem",
+        labels={
+            "odometer": "Quilometragem (km)",
+            "price": "Preço (R$)",
+            "condition": "Condição"
+        },
+        opacity=0.7,
+    )
     st.plotly_chart(fig, use_container_width=True)
